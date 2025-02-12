9f495d5fea48370aa92cdf3b139ea9793b435966
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-02-12 13:40:23 +0100

    Improve the atomic test

--- pkg/move/atomic_test.go
+++ pkg/move/atomic_test.go
@@ -2,16 +2,32 @@ package move
 
 import (
 	"errors"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func mockCopyFunc(isSuccessful bool) copyFunc {
+func mockCopyFuncWithAtomicCheck(t *testing.T, isSuccessful bool) copyFunc {
+	t.Helper()
+
 	return func(fs afero.Afero, _, targetFolder string) error {
+
+		// according to the inner copyFunc, the targetFolder should be the workFolder
+		// the actual targetFolder will be created outside the copyFunc by the atomic wrapper using fs.Rename
+		require.Equal(t, workFolder, targetFolder)
+
+		// the atomicWrapper should already have created the base workFolder
+		exists, err := fs.DirExists(targetFolder)
+		require.NoError(t, err)
+		require.True(t, exists)
+
 		if isSuccessful {
-			_ = fs.MkdirAll(targetFolder, 0755)
+			file, err := fs.Create(filepath.Join(targetFolder, "test.txt"))
+			require.NoError(t, err)
+			file.Close()
 			return nil
 		}
 
@@ -28,18 +44,22 @@ func TestAtomic(t *testing.T) {
 		err := fs.MkdirAll(sourceFolder, 0755)
 		assert.NoError(t, err)
 
-		atomicCopy := atomic(mockCopyFunc(true))
+		atomicCopy := atomic(mockCopyFuncWithAtomicCheck(t, true))
 
 		err = atomicCopy(fs, sourceFolder, targetFolder)
 		assert.NoError(t, err)
 
-		exists, err := fs.DirExists(workFolder)
+		isEmpty, err := fs.DirExists(workFolder)
 		assert.NoError(t, err)
-		assert.False(t, exists)
+		assert.False(t, isEmpty)
 
-		exists, err = fs.DirExists(targetFolder)
+		isEmpty, err = fs.DirExists(targetFolder)
 		assert.NoError(t, err)
-		assert.True(t, exists)
+		assert.True(t, isEmpty)
+
+		isEmpty, err = fs.IsEmpty(targetFolder)
+		assert.NoError(t, err)
+		assert.False(t, isEmpty)
 	})
 	t.Run("fail -> targetFolder is not present", func(t *testing.T) {
 		fs := afero.Afero{Fs: afero.NewMemMapFs()}
@@ -47,7 +67,7 @@ func TestAtomic(t *testing.T) {
 		targetFolder = "/target"
 		workFolder = "/work"
 
-		atomicCopy := atomic(mockCopyFunc(false))
+		atomicCopy := atomic(mockCopyFuncWithAtomicCheck(t, false))
 
 		err := atomicCopy(fs, sourceFolder, targetFolder)
 		assert.Error(t, err)
