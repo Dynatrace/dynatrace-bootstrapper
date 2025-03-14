3a4b721f91af39fe235d615f9759cb7b3513d425
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-03-14 14:19:34 +0100

    Fix the build

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"os"
 
-	"github.com/Dynatrace/dynatrace-bootstrapper/cmd"
+	bootstrapper "github.com/Dynatrace/dynatrace-bootstrapper/cmd"
 	"github.com/spf13/afero"
 )
 
 func main() {
-	err := cmd.New(afero.NewOsFs()).Execute()
+	err := bootstrapper.New(afero.NewOsFs()).Execute()
 	if err != nil {
 		os.Exit(1)
 	}
