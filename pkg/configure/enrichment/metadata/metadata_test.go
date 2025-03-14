85a26be5f2959f96efc93d93ff8a56cb487fd9a1
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-03-14 13:25:52 +0100

    Add missing members to pod.Attributes Also some minor renames for consistency

--- pkg/configure/enrichment/metadata/metadata_test.go
+++ pkg/configure/enrichment/metadata/metadata_test.go
@@ -24,11 +24,11 @@ func TestConfigure(t *testing.T) {
 		},
 		PodInfo: pod.PodInfo{
 			PodName:       "podname",
-			PodUid:        "poduid",
+			PodUID:        "poduid",
 			NamespaceName: "namespacename",
 		},
 		ClusterInfo: pod.ClusterInfo{
-			ClusterUId:      "clusteruid",
+			ClusterUID:      "clusteruid",
 			DTClusterEntity: "dtclusterentity",
 		},
 		WorkloadInfo: pod.WorkloadInfo{
