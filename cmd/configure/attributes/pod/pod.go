7a986739ea886e465d0cbc9a22fe9381bb8b80bd
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-05-02 14:24:08 +0200

    Add omitempty

--- cmd/configure/attributes/pod/pod.go
+++ cmd/configure/attributes/pod/pod.go
@@ -39,7 +39,7 @@ type ClusterInfo struct {
 	ClusterUID      string `json:"k8s.cluster.uid"`
 	ClusterName     string `json:"k8s.cluster.name"`
 	DTClusterEntity string `json:"dt.entity.kubernetes_cluster"`
-	DTTenantUID     string `json:"dt.tenant"`
+	DTTenantUID     string `json:"dt.tenant,omitempty"`
 }
 
 func ParseAttributes(rawAttributes []string) (Attributes, error) {
